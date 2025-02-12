8e30bca51256e4e013ca8f080afeaebcae127bac
Benedikt_MBP <provers.0-pluses@icloud.com>
2025-02-12 23:35:44 +0000

    first two problems solved.

--- labs109.py
+++ labs109.py
@@ -17,3 +17,34 @@ def ryerson_letter_grade(n):
         adjust = ""
     return "DCB"[tens - 5] + adjust
 
+
+# Started 11.02.2025 - BOP - SUAI
+def is_ascending(items):
+
+    if len(items) < 2:
+        return True
+    
+    for i in range(len(items) - 1):
+        if items[i] >= items[i + 1]:
+            return False
+
+    return True
+    
+
+# Started 11.02.2025 - BOP
+def riffle(items, out=True):
+    
+    if len(items) > 1:
+        h = len(items) // 2
+        split_list1 = items[:h]
+        split_list2 = items[h:]
+        result = []
+        for n in range(len(items)//2):
+            result.append(split_list1[n])
+            result.append(split_list2[n])
+    elif len(items) == 2:
+        result.append(items[1])
+        result.append(items[0])
+    
+    else:
+        return items
